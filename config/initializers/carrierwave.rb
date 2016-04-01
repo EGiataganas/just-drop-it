b58d77e5b7c1992d0e30b07464d8c15d48754da0
Evangelos Giataganas <e.giataganas@gmail.com>
2016-04-01 02:17:49 +0100

    update carrierwave configuration to use aws only on production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
 CarrierWave.configure do |config|
-  if ENV['S3_KEY'] && ENV['S3_SECRET'] && ENV['S3_BUCKET_NAME']
+  if Rails.env.production?
     config.storage = :fog
     config.fog_credentials = {
-      provider:              ENV['S3_PROVIDER'],
-      aws_access_key_id:     ENV['S3_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET'],
-      region:                ENV['S3_REGION']
+      provider:              Figaro.env.PROVIDER,
+      aws_access_key_id:     Figaro.env.AWS_KEY,
+      aws_secret_access_key: Figaro.env.AWS_SECRET,
+      region:                Figaro.env.AWS_REGION
     }
-    config.fog_directory = ENV['S3_BUCKET_NAME']
+    config.fog_directory = Figaro.env.S3_BUCKET_NAME
   elsif Rails.env.test?
     config.storage = :file
     config.enable_processing = false
