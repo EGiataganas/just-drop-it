c628288c4378bcea78b8b90bf1e4c7ad1c6faf79
sofo01 <sofoklis_the@hotmail.com>
2016-04-02 23:41:14 +0100

    fix rubocop offenses

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # Require for Device. Remember to change localhost:3000 to actual application host
+  # Require for Device. Remember to change localhost:3000 to actual
+  # application host
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
